f15cf9bb6cbcf86e26913a5dffe498552f76798d
Damian Malczewski <damian.m.malczewski@gmail.com>
2021-10-07 23:34:36 +0200

    Add ThingMapper test for mapping one entity

---
+++ src/test/java/io/github/malczuuu/shadowthings/core/ThingMapperTests.java
@@ -0,0 +1,42 @@
+package io.github.malczuuu.shadowthings.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import io.github.malczuuu.shadowthings.entity.ThingEntity;
+import io.github.malczuuu.shadowthings.model.ThingModel;
+import java.time.Instant;
+import java.util.UUID;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ThingMapperTests {
+
+  private ThingMapper thingMapper;
+
+  @BeforeEach
+  void beforeEach() {
+    thingMapper = new ThingMapper();
+  }
+
+  @Test
+  void shouldMapThingEntity() {
+    ThingEntity entity =
+        new ThingEntity(
+            ObjectId.get(),
+            UUID.randomUUID().toString(),
+            "an example name",
+            false,
+            "password",
+            Instant.parse("2020-05-21T06:53:41.443Z"),
+            12L);
+
+    ThingModel model = thingMapper.toModel(entity);
+
+    assertEquals(entity.getUid(), model.getId());
+    assertEquals(entity.getName(), model.getName());
+    assertEquals(entity.isEnabled(), model.isEnabled());
+    assertEquals(entity.getLastModifiedDate().toString(), model.getLastModifiedDate());
+    assertEquals(entity.getVersion(), model.getVersion());
+  }
+}
