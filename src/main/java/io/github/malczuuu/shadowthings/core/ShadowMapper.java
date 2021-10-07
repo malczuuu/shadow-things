85ab4c441c5a97fdccb51b2fc4afdec390de4a7a
Damian Malczewski <damian.m.malczewski@gmail.com>
2021-10-07 23:25:33 +0200

    Add ShadowMapper

---
+++ src/main/java/io/github/malczuuu/shadowthings/core/ShadowMapper.java
@@ -0,0 +1,20 @@
+package io.github.malczuuu.shadowthings.core;
+
+import io.github.malczuuu.shadowthings.entity.ShadowEntity;
+import io.github.malczuuu.shadowthings.model.ShadowModel;
+import java.util.HashMap;
+
+public class ShadowMapper {
+
+  private final TimeMapper timeMapper = new TimeMapper();
+
+  public ShadowModel toModel(ShadowEntity shadow) {
+    return new ShadowModel(
+        new HashMap<>(shadow.getDesired()),
+        new HashMap<>(shadow.getReported()),
+        shadow.getLastModifiedDate() != null
+            ? timeMapper.toIsoString(shadow.getLastModifiedDate())
+            : null,
+        shadow.getVersion());
+  }
+}
