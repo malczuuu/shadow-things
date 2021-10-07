3b888faeb86874bf8269025598a249ee32a5ab2d
Damian Malczewski <damian.m.malczewski@gmail.com>
2021-10-07 23:22:35 +0200

    Add UpdateShadowModel

---
+++ src/main/java/io/github/malczuuu/shadowthings/model/UpdateShadowModel.java
@@ -0,0 +1,33 @@
+package io.github.malczuuu.shadowthings.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Map;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+public final class UpdateShadowModel {
+
+  private final Map<String, Object> desired;
+  private final Long version;
+
+  @JsonCreator
+  public UpdateShadowModel(
+      @JsonProperty("desired") Map<String, Object> desired, @JsonProperty("version") Long version) {
+    this.desired = desired;
+    this.version = version;
+  }
+
+  @NotNull
+  @JsonProperty("desired")
+  public Map<String, Object> getDesired() {
+    return desired;
+  }
+
+  @NotNull
+  @Min(0)
+  @JsonProperty("version")
+  public Long getVersion() {
+    return version;
+  }
+}
