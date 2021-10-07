da56a9bc0fc26bbefdf96c8bc4502de2de603b1d
Damian Malczewski <damian.m.malczewski@gmail.com>
2021-10-07 23:23:32 +0200

    Add ConcurrentUpdateException

---
+++ src/main/java/io/github/malczuuu/shadowthings/core/ConcurrentUpdateException.java
@@ -0,0 +1,17 @@
+package io.github.malczuuu.shadowthings.core;
+
+import io.github.malczuuu.problem4j.core.Problem;
+import io.github.malczuuu.problem4j.core.ProblemException;
+import org.springframework.http.HttpStatus;
+
+public class ConcurrentUpdateException extends ProblemException {
+
+  public ConcurrentUpdateException() {
+    super(
+        Problem.builder()
+            .title(HttpStatus.CONFLICT.getReasonPhrase())
+            .status(HttpStatus.CONFLICT.value())
+            .detail("Requested version not up to date")
+            .build());
+  }
+}
