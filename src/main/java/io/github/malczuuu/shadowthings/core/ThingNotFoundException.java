f0bd17d3213a41c42fbb26c14d6ab93d212c7263
Damian Malczewski <damian.m.malczewski@gmail.com>
2021-10-07 23:25:58 +0200

    Add ThingNotFoundException

---
+++ src/main/java/io/github/malczuuu/shadowthings/core/ThingNotFoundException.java
@@ -0,0 +1,17 @@
+package io.github.malczuuu.shadowthings.core;
+
+import io.github.malczuuu.problem4j.core.Problem;
+import io.github.malczuuu.problem4j.core.ProblemException;
+import org.springframework.http.HttpStatus;
+
+public class ThingNotFoundException extends ProblemException {
+
+  public ThingNotFoundException(String id) {
+    super(
+        Problem.builder()
+            .title(HttpStatus.NOT_FOUND.getReasonPhrase())
+            .status(HttpStatus.NOT_FOUND.value())
+            .detail("Thing \"" + id + "\" not found")
+            .build());
+  }
+}
